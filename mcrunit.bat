6109e7bdb2cf7a12fc0f77299d4f0f9e4ef4933f
melomel07 <90223564+melomel07@users.noreply.github.com>
2025-07-04 21:17:28 +0100

    Update mcrunit.bat
    - MCrunit now checks for server files, no need for user input
    - Removed runarea, runall, runbot & runproxy, no need
    - New simpler user input thingy
    - Simpler "title" (where version is shown)
    - Changed window size
    - Added colored text!
    - File Size reduced drastically, nice! x3

--- mcrunit.bat
+++ mcrunit.bat
@@ -1,28 +1,29 @@
 @echo off
-title ARE YOU DREAMING? [v2.0]
+mode con: cols=61 lines=22
+title [v2.0]
 set "filename=%~f0"
-setlocal
+setlocal EnableDelayedExpansion
 set "keywords=server arclight paper spigot bukkit glowstone sponge purpur pufferfish magma mohist cat playit"
 set "searchDir=%~dp0"
 
 :mainarea
 cls
 
-echo                 __    __     ______    
-echo                /\ "-./  \   /\  ___\   
-echo                \ \ \-./\ \  \ \ \____  
-echo                 \ \_\ \ \_\  \ \_____\ 
-echo                  \/_/  \/_/   \/_____/ 
-echo    ______     __  __     __   __        __     ______  
-echo   /\  == \   /\ \/\ \   /\ "-.\ \      /\ \   /\__  _\ 
-echo   \ \  __-   \ \ \_\ \  \ \ \-.  \     \ \ \  \/_/\ \/ 
-echo    \ \_\ \_\  \ \_____\  \ \_\\"\_\     \ \_\    \ \_\ 
-echo     \/_/ /_/   \/_____/   \/_/ \/_/      \/_/     \/_/ 
+echo                   __    __     ______    
+echo                  /\ "-./  \   /\  ___\   
+echo                  \ \ \-./\ \  \ \ \____  
+echo                   \ \_\ \ \_\  \ \_____\ 
+echo                    \/_/  \/_/   \/_____/ 
+echo      ______     __  __     __   __        __     ______  
+echo     /\  == \   /\ \/\ \   /\ "-.\ \      /\ \   /\__  _\ 
+echo     \ \  __-   \ \ \_\ \  \ \ \-.  \     \ \ \  \/_/\ \/ 
+echo      \ \_\ \_\  \ \_____\  \ \_\\"\_\     \ \_\    \ \_\ 
+echo       \/_/ /_/   \/_____/   \/_/ \/_/      \/_/     \/_/ 
 echo.
 echo  - Type "configarea" to configure the server to your liking.
-echo  - Type "runarea" to run this server.
+echo  - To run the server, type "run".
 echo.
-echo  -----------------------Notes-----------------------
+echo  ---------------------------Notes---------------------------
 echo.
 echo  - While running, type "help" for a list of all commands.
 echo  - While running, type "exit" to save and stop the server.
@@ -30,15 +31,11 @@ echo  - You can't close all the windows with the "stop"
 echo    command so you have to manually close them :P
 echo.
 
-set /p option1= [%username%]~ 
+set /p option1= ">> "
 if %option1% == configarea goto configarea
-if %option1% == runarea goto runarea
 if %option1% == mainarea goto mainarea
 if %option1% == info goto info
-if %option1% == runall goto runall
 if %option1% == run goto run
-if %option1% == runproxy goto runproxy
-if %option1% == runbot goto runbot
 if %option1% == changeram goto changeram
 
 goto mainarea
@@ -48,21 +45,17 @@ pause
 cls
 
 echo.
-echo      TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
+echo          TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
 echo.
 echo  - Type "changeram" to change the ammount of ram you
 echo    want to allocate to your server
 echo.
 
-set /p option1= [%username%]~ 
+set /p option1= ">> "
 if %option1% == configarea goto configarea
-if %option1% == runarea goto runarea
 if %option1% == mainarea goto mainarea
 if %option1% == info goto info
-if %option1% == runall goto runall
 if %option1% == run goto run
-if %option1% == runproxy goto runproxy
-if %option1% == runbot goto runbot
 if %option1% == changeram goto changeram
 
 goto mainarea
@@ -90,94 +83,21 @@ start /B /c "%~f0" & goto configarea
 
 pause
 
-:runarea
-cls
-
-echo.
-echo      TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
-echo.
-echo  - To run all the stuff needed for the server, Type "runall"
-echo    (If you don't have a discord bot or playit proxy
-echo    you might get errors, it's OK. The server will
-echo    still work :D )
-echo  - To run only the local server, Type "run"
-echo  - To run only the playit proxy, Type "runproxy"
-echo  - To run only the discord bot, Type "runbot"
-echo.
-
-set /p option1= [%username%]~ 
-if %option1% == configarea goto configarea
-if %option1% == runarea goto runarea
-if %option1% == mainarea goto mainarea
-if %option1% == info goto info
-if %option1% == runall goto runall
-if %option1% == run goto run
-if %option1% == runproxy goto runproxy
-if %option1% == runbot goto runbot
-if %option1% == changeram goto changeram
-
-goto mainarea
-pause
-
 :info
 cls
 
-echo    TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
+echo          TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
 echo.
 echo.
 echo  - https://github.com/melomel07/mcrunit
 echo.
 echo.
 
-set /p option1= [%username%]~ 
+set /p option1= ">> "
 if %option1% == configarea goto configarea
-if %option1% == runarea goto runarea
 if %option1% == mainarea goto mainarea
 if %option1% == info goto info
-if %option1% == runall goto runall
 if %option1% == run goto run
-if %option1% == runproxy goto runproxy
-if %option1% == runbot goto runbot
-if %option1% == changeram goto changeram
-
-goto mainarea
-pause
-
-:runall
-cls
-
-echo.
-echo   Please be patient, this may take a bit!
-echo.
-
-start "..\Python\Python311\python.exe" "dbot.py"
-
-for %%k in (%keywords%) do (
-	for %%f in ("%searchDir%\*%%k*.exe") do (
-		start %%f
-	)
-)
-
-for %%k in (%keywords%) do (
-	for %%f in ("%searchDir%\*%%k*.jar") do (
-		java -Xmx6G -Xms5G -jar "%%f" nogui 
-	)
-)
-
-echo.
-
-echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
-echo.
-
-set /p option1= [%username%]~ 
-if %option1% == configarea goto configarea
-if %option1% == runarea goto runarea
-if %option1% == mainarea goto mainarea
-if %option1% == info goto info
-if %option1% == runall goto runall
-if %option1% == run goto run
-if %option1% == runproxy goto runproxy
-if %option1% == runbot goto runbot
 if %option1% == changeram goto changeram
 
 goto mainarea
@@ -187,79 +107,56 @@ pause
 cls
 
 echo.
-echo   Please be patient, this may take a bit!
+echo.
+echo            Please be patient, this may take a bit!
+echo.
 echo.
 
-for %%k in (%keywords%) do (
-	for %%f in ("%searchDir%\*%%k*.jar") do (
-		java -Xmx6G -Xms5G -jar "%%f" nogui 
-	)
+if exist "dbot.py" (
+	start "..\Python\Python311\python.exe" "dbot.py"
+	echo.
+) else (
+	powershell -Command "Write-Host 'No Discord bot file found, continuing anyways...' -ForegroundColor Yellow"
+	echo.
 )
 
-echo.
-
-echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
-echo.
-
-set /p option1= [%username%]~ 
-if %option1% == configarea goto configarea
-if %option1% == runarea goto runarea
-if %option1% == mainarea goto mainarea
-if %option1% == info goto info
-if %option1% == runall goto runall
-if %option1% == run goto run
-if %option1% == runproxy goto runproxy
-if %option1% == runbot goto runbot
-if %option1% == changeram goto changeram
-
-goto mainarea
-pause
-
-:runproxy
-cls
-
+set "found=0"
 for %%k in (%keywords%) do (
 	for %%f in ("%searchDir%\*%%k*.exe") do (
-		start %%f
+		if exist "%%f" (
+        set "found=1"
+        start %%f
+		)
 	)
 )
+if !found! EQU 0 (
+    powershell -Command "Write-Host 'No Playit app file found, continuing anyways...' -ForegroundColor Yellow"
+    echo.
+)
 
-echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
+set "found2=0"
+for %%k in (%keywords%) do (
+	for %%f in ("%searchDir%\*%%k*.jar") do (
+		if exist "%%f" (
+        set "found2=1"
+		java -Xmx6G -Xms5G -jar "%%f" nogui 
+		)
+	)
+)
+if !found2! EQU 0 (
+    powershell -Command "Write-Host 'No server files found.' -ForegroundColor Red"
+    echo.
+)
+
+echo.
+echo          TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
 echo.
 
-set /p option1= [%username%]~ 
+set /p option1= ">> "
 if %option1% == configarea goto configarea
-if %option1% == runarea goto runarea
 if %option1% == mainarea goto mainarea
 if %option1% == info goto info
-if %option1% == runall goto runall
 if %option1% == run goto run
-if %option1% == runproxy goto runproxy
-if %option1% == runbot goto runbot
-if %option1% == changeram goto changeram
-
-goto mainarea
-pause
-
-:runbot
-cls
-
-echo.
-
-start "..\Python\Python311\python.exe" "dbot.py"
-
-echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
-echo.
-
-set /p option1= [%username%]~ 
-if %option1% == configarea goto configarea
-if %option1% == runarea goto runarea
-if %option1% == mainarea goto mainarea
-if %option1% == info goto info
-if %option1% == runall goto runall
-if %option1% == run goto run
-if %option1% == runproxy goto runproxy
-if %option1% == runbot goto runbot
 if %option1% == changeram goto changeram
 
 goto mainarea
